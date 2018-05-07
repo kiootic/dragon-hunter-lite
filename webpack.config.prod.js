e7a533e7a9896cb6ec1c2bd9ece5a48b3267f525
kiootic <kiootic@gmail.com>
2018-05-07 15:35:45 +0800

    Fix minification bug

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,7 +5,17 @@ const config = require('./webpack.config');
 module.exports = Object.assign(config, {
   mode: 'production',
   devtool: 'source-map',
-  optimization: Object.assign(config.optimization, { minimizer: [new UglifyJsPlugin({ parallel: 8, sourceMap: true })] }),
+  optimization: Object.assign(config.optimization, {
+    minimizer: [new UglifyJsPlugin({
+      parallel: 8,
+      sourceMap: true,
+      uglifyOptions:{
+        compress: {
+          inline: false // Workaround for UglifyJS2#2842
+        }
+      }
+    })]
+  }),
   plugins: config.plugins.concat([
     new CleanWebpackPlugin(['dist'])
   ])
